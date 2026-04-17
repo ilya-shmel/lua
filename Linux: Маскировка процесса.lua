dc12445c0ec7e45abc2c5cedfa2df4e97e336e56
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-04-17 12:03:49 +0300

    a574c2ff-1329-49d6-a1ef-078a95c434c6 - the rule works without errors.

--- Linux: Маскировка процесса.lua
+++ Linux: Маскировка процесса.lua
@@ -10,21 +10,26 @@ FQDN: {{.Meta.observer_fqdn}}
 Пользователь: {{.Meta.user_name}}
 UID: {{.Meta.user_id}}
 Процесс: {{.Meta.process_path}}
-PID: {{.Meta.parent_pid}} -> {{.Meta.child_pid}}
+Идентификатор родительского процесса: {{.Meta.parent_pid}}
+Идентификатор "дочернего" процесса: {{.Meta.child_pid}}
 
 ВЫПОЛНЕННАЯ КОМАНДА:
 {{.Meta.command}}
 ]]
 
-local detection_window = "3s"
-local grouped_by = {"observer.host.ip", "observer.host.hostname", "@timestamp"}
+local detection_window = "1m"
+local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn"}
 local aggregated_by = {"observer.event.type"}
 local grouped_time_field = "@timestamp,RFC3339"
 
-local suspicious_patterns = {"sh\\s+-c\\s+\\./[a-zA-Z0-9]{6,}", "bash\\s+-c\\s+\\./[a-zA-Z0-9]{6,}",
-                             "sh\\s+-c\\s+[a-zA-Z0-9/]{1,}\\s+\\|", "bash\\s+-c\\s+[a-zA-Z0-9/]{1,}\\s+\\|",
-                             "sh\\s+-c\\s+\\$\\{.*\\}", "bash\\s+-c\\s+\\$\\{.*\\}", "\\./[a-zA-Z0-9]{8,}",
-                             "\\$\\(.*\\)", "exec\\s+", ">\\s*/dev/null"}
+local suspicious_patterns = {   
+                                "[\\s\'\"(]+while\\s+true;(\\s+do(ne)?[\\s;:)]+){2}&[\'\";\\s]+\\w+\\s+\\$!\\s+[>]+\\s+[\\/\\w\\.]+;[\\s\\S]*?[$(\\s]+ps[-\\s\\w]+\\|\\s+grep[$(\\s]+[\\\\,\'\"\\.\\*\\[\\]\\s]+\\|\\s+awk\\s+[\'\"{}$\\s\\w]+\\|\\s+shuf[-\\w\\s);]+mount[-\\w\\s]+([\\/\\w$()\\.\\s]+){1,2}",
+                                "[-\\w\\/]+\\s+&\\s+(?:ps|lsof|ss|\\w+?top|pgrep|fuser|systemctl|uhide(-tcp))",
+                                "(ba)?sh\\s+-c\\s+(?:\\./[a-zA-Z0-9]{6,}|[a-zA-Z0-9/]{1,}\\s+\\||\\$\\{.*\\})",
+                                "\\./[a-zA-Z0-9]{8,}",
+                                "\\$\\(.*\\)", "exec\\s+",
+                                ">\\s*/dev/null"
+}
 
 local legitimate_patterns = {"ansible", "puppet", "chef", "salt", "docker", "kubernetes", "systemd", "cron", "scripts"}
 
@@ -51,14 +56,14 @@ local function analyze(cmd)
 end
 
 function on_logline(logline)
-    local event_type = logline:gets("observer.event.type") or ""
+    local event_type = logline:gets("observer.event.type")
 
     if event_type == "SYSCALL" then
-        if logline:gets("target.syscall.name") == "vfork" then
-            grouper1:feed(logline)
-        end
+        log("Sending Syscall to grouper!")
+        grouper1:feed(logline)
     elseif event_type == "EXECVE" then
-        local cmd = logline:gets("initiator.command.executed") or ""
+        log("Sending Esecve to grouper!")
+        local cmd = logline:gets("initiator.command.executed")
         if analyze(cmd) then
             grouper1:feed(logline)
         end
@@ -67,57 +72,83 @@ end
 
 function on_grouped(grouped)
     local events = grouped.aggregatedData.loglines
-    local log_execve = ""
-    local log_vfork = ""
+    local unique_events = grouped.aggregatedData.unique.total
+    local log_execve = nil
+    local log_vfork = nil
+    local log_syscall = nil
     local vfork_count = 0
+    local syscall_count = 0
 
-    for _, log in ipairs(events) do
-        local event_type = log:gets("observer.event.type") or ""
-        if event_type == "SYSCALL" and log:gets("target.syscall.name") == "vfork" then
-            log_vfork = log
-            vfork_count = vfork_count + 1
-        elseif event_type == "EXECVE" then
-            log_execve = log
+    if unique_events > 1 then
+        local type1 = events[1]:gets("observer.event.type")
+        local type2 = events[2]:gets("observer.event.type")
+        local syscall_name1 = events[1]:gets("target.syscall.name", "Not SYSCALL")
+        local syscall_name2 = events[2]:gets("target.syscall.name", "Not SYSCALL")
+
+        log("Events: " ..#events.. ". Unique events: " ..unique_events)
+        log("Type 1: " ..type1.. ". Type 2: " ..type2)
+        log("Syscall name1: " ..syscall_name1.. ". Syscall name2: " ..syscall_name2)
+        
+        for _, log in ipairs(events) do
+            local event_type = log:gets("observer.event.type")
+                        
+            if event_type == "SYSCALL" then
+                local syscall_name = log:gets("target.syscall.name")
+                
+                if syscall_name == "vfork" then
+                    log_vfork = log
+                    vfork_count = vfork_count + 1
+                else
+                    log_syscall = log
+                    syscall_count = syscall_count + 1
+                end
+            elseif event_type == "EXECVE" then
+                log_execve = log
+            end
         end
-    end
 
-    if log_execve ~= "" and log_vfork ~= "" and vfork_count > 0 then
-        local parent_pid = log_vfork:gets("initiator.process.parent.id") or ""
-        local child_pid = log_vfork:gets("initiator.process.id") or ""
-        local command = log_execve:gets("initiator.command.executed") or ""
-        local process_path = log_vfork:gets("initiator.process.path.full") or ""
-        local user_name = log_vfork:gets("initiator.user.name") or "unknown"
-        local user_id = log_vfork:gets("initiator.user.id") or "unknown"
+        if log_execve and log_vfork and log_syscall and vfork_count > 0 and syscall_count > 0 then
+            local initiator_id = log_vfork:gets("initiator.process.id")
+            local target_pid = log_syscall:gets("initiator.process.id")
+            local parent_pid = log_syscall:gets("initiator.process.parent.id")
+            
+            if initiator_id == target_pid then
+                local command = log_execve:gets("initiator.command.executed")
+                local process_path = log_vfork:gets("initiator.process.path.full")
+                local user_name = log_vfork:gets("initiator.user.name") or "Имя пользователя не определено"
+                local user_id = log_vfork:gets("initiator.user.id") or "Идентификатор пользователя не определен"
 
-        alert({
-            template = template,
-            meta = {
-                observer_ip = log_execve:gets("observer.host.ip") or "",
-                observer_hostname = log_execve:gets("observer.host.hostname") or "",
-                observer_fqdn = log_execve:gets("observer.host.fqdn") or "",
-                user_name = user_name,
-                user_id = user_id,
-                process_path = process_path,
-                command = command,
-                parent_pid = parent_pid,
-                child_pid = child_pid,
-                suspicious_path = command,
-                detection_count = tostring(vfork_count)
-            },
-            risk_level = 7.5,
-            asset_ip = log_execve:get_asset_data("observer.host.ip"),
-            asset_hostname = log_execve:get_asset_data("observer.host.hostname"),
-            asset_fqdn = log_execve:get_asset_data("observer.host.fqdn"),
-            asset_mac = "",
-            create_incident = true,
-            incident_group = "",
-            assign_to_customer = false,
-            incident_identifier = "",
-            logs = grouped.aggregatedData.loglines,
-            mitre = {"T1036.004", "T1578.004", "T1564.007"},
-            trim_logs = 10
-        })
-        grouper1:clear()
+                   alert({
+                    template = template,
+                    meta = {
+                        observer_ip = log_execve:gets("observer.host.ip"),
+                        observer_hostname = log_execve:gets("observer.host.hostname"),
+                        observer_fqdn = log_execve:gets("observer.host.fqdn"),
+                        user_name = user_name,
+                        user_id = user_id,
+                        process_path = process_path,
+                        command = command,
+                        parent_pid = initiator_pid,
+                        child_pid = parent_pid,
+                        suspicious_path = command,
+                        detection_count = tostring(vfork_count)
+                    },
+                    risk_level = 7.5,
+                    asset_ip = log_execve:get_asset_data("observer.host.ip"),
+                    asset_hostname = log_execve:get_asset_data("observer.host.hostname"),
+                    asset_fqdn = log_execve:get_asset_data("observer.host.fqdn"),
+                    asset_mac = "",
+                    create_incident = true,
+                    incident_group = "",
+                    assign_to_customer = false,
+                    incident_identifier = "",
+                    logs = events,
+                    mitre = {"T1036.004", "T1578.004", "T1564.007"},
+                    trim_logs = 10
+                })
+                grouper1:clear()
+            end
+        end
     end
 end
 
