f10cf07515b6386f8c66a6c82c66c373589e792e
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-06-18 16:00:04 +0300

    dc381993-6ac7-4b38-9611-2eeac470882c - was edited.

--- T1059.001 - Windows-Eventlog: Запуск команд PowerShell с использованием XML.lua
+++ T1059.001 - Windows-Eventlog: Запуск команд PowerShell с использованием XML.lua
@@ -11,15 +11,48 @@ IP-адрес: {{ .Meta.host_ip }}
 Тип задействованного объекта: {{ .Meta.object }}
 ]]
 
--- Параметры группера
-local detection_window = "30s"
-local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.process.id"}
-local aggregated_by = {"initiator.process.command"}
+-- Параметры группера #1
+local detection_window1 = "30s"
+local grouped_by1 = {"observer.host.ip", "observer.host.hostname", "observer.process.id"}
+local aggregated_by1 = {"initiator.process.command"}
+
+-- Параметры группера #2
+local detection_window2 = "30s"
+local grouped_by2 = {"observer.host.ip", "observer.host.hostname", "event.process.id"}
+local aggregated_by2 = {"observer.event.id"}
+
 local grouped_time_field = "@timestamp,RFC3339"
 
 -- Регулярные выражения, шаблоны
 local invoke_patterns = { "io.compression.deflatestream", "system.collections.generic.list[system.object]" }
 
+-- Функция алерта
+local function alert_function(ip, hostname, fqdn, user, cmd, xml_cmd, object, events, template)
+    alert({
+            template = template,
+            meta = {
+                user_name=user,
+                command=cmd,
+                xml_command=xml_cmd,
+                object=object,
+                host_ip=ip,
+                hostname=hostname
+                },
+            risk_level = 6.0, 
+            asset_ip = ip,
+            asset_hostname = hostname,
+            asset_fqdn = fqdn,
+            asset_mac = "",
+            create_incident = true,
+            incident_group = "",
+            assign_to_customer = false,
+            incident_identifier = "",
+            logs = events,
+            mitre = {"T1059.001"},
+            trim_logs = 10
+            }
+        )
+end
 
 -- Функция выделения уникальных значений в таблице
 local function unique_elements(commands_table)
@@ -36,11 +69,28 @@ end
 
 -- Функция работы с логлайном
 function on_logline(logline)
-    grouper1:feed(logline)
+    local event_id = logline:gets("observer.event.id")
+
+    if compare(event_id, "==", "4688") then
+        local target_pid = logline:gets("target.process.id")
+        local process_id = tonumber(target_pid:gsub("^0[xX]", ""), 16) -- Преобразование из шестнадцатеричной системы в десятичную
+        set_field_value(logline, "event.process.id", process_id)
+        grouper2:feed(logline)
+    elseif compare(event_id, "==", "4103") then
+        local object_name =  logline:gets("target.object.name"):lower()
+
+        if object_name:find("msxml2") or object_name:find("serverxmlhttp") then
+            local process_id = logline:gets("observer.process.id")
+            set_field_value(logline, "event.process.id", process_id)
+            grouper2:feed(logline)        
+        else
+            grouper1:feed(logline)
+        end
+    end
 end
 
 -- Функция сработки группера
-function on_grouped(grouped)
+function on_grouped1(grouped)
     local events = grouped.aggregatedData.loglines
     local unique_events = grouped.aggregatedData.unique.total
     local log_exec = nil
@@ -69,37 +119,47 @@ function on_grouped(grouped)
             local host_ip = log_exec:get("observer.host.ip") or log_exec:gets("reportchain.collector.host.ip", "IP-адрес не определён")
             local host_name = log_exec:gets("observer.host.hostname", "Имя узла не определено")
             local host_fqdn = log_exec:gets("observer.host.fqdn", "FQDN узла не определено")
-            local xml_command = log_exec:get("target.object.name")
-            local object_name = log_object:get("target.object.name")
+            local xml_command = log_exec:gets("target.object.name")
+            local object_name = log_object:gets("target.object.name")
 
-            alert({
-               template = template,
-               meta = {
-                   user_name=initiator_name,
-                   command=all_commands,
-                   xml_command=xml_command,
-                   object=object_name,
-                   host_ip=host_ip,
-                   hostname=host_name
-                   },
-               risk_level = 6.0, 
-               asset_ip = host_ip,
-               asset_hostname = host_name,
-               asset_fqdn = host_fqdn,
-               asset_mac = "",
-               create_incident = true,
-               incident_group = "",
-               assign_to_customer = false,
-               incident_identifier = "",
-               logs = events,
-               mitre = {"T1059.001"},
-               trim_logs = 10
-               }
-            )
+            alert_function(host_ip, host_name, host_fqdn, initiator_name, all_commands, xml_command, object_name, events, template)
             grouper1:clear()
         end
     end
 end
 
+function on_grouped2(grouped)
+    local events = grouped.aggregatedData.loglines
+    local unique_events = grouped.aggregatedData.unique.total
+    local log_exec = nil
+    local log_module = nil
+
+    if unique_events > 1 then
+        for _, event in ipairs(events) do
+            local event_id = event:gets("observer.event.id")
+            
+            if compare(event_id, "==", "4688") then
+                log_exec = event
+            else
+                log_module = event
+            end
+        end
+
+        if log_module and log_exec then 
+            local command_executed = log_exec:gets("initiator.command.executed")
+            local initiator_name = log_module:gets("initiator.user.name", "Пользователь не определён")  
+            local host_ip = log_exec:get("observer.host.ip") or log_exec:gets("reportchain.collector.host.ip", "IP-адрес не определён")
+            local host_name = log_exec:gets("observer.host.hostname", "Имя узла не определено")
+            local host_fqdn = log_exec:gets("observer.host.fqdn", "FQDN узла не определено")
+            local xml_command = log_module:gets("initiator.command.executed")
+            local object_name = log_module:gets("target.object.name")
+
+            alert_function(host_ip, host_name, host_fqdn, initiator_name, all_commands, xml_command, object_name, events, template)
+            grouper2:clear()
+        end
+    end
+end
+
 -- Группер
-grouper1 = grouper.new(grouped_by, aggregated_by, grouped_time_field, detection_window, on_grouped)
+grouper1 = grouper.new(grouped_by1, aggregated_by1, grouped_time_field, detection_window1, on_grouped1)
+grouper2 = grouper.new(grouped_by2, aggregated_by2, grouped_time_field, detection_window2, on_grouped2)
