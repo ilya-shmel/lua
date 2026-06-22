a1e8a70b2e5fa43612b8959481e5c218eebd8504
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-06-22 09:49:00 +0300

    Rule 0a15d20e-84cb-438c-8c36-cf4326fb1bb4 - first part.

--- Windows: Использование Mimikatz2.lua
+++ Windows: Использование Mimikatz2.lua
@@ -3,17 +3,18 @@ local template = [[
 Подозрение на запуск инструмента Mimikatz.
 
 ЦЕЛЕВОЙ УЗЕЛ:
-IP-адрес: {{ .Meta.host_ip }}
+IP-адрес: {{ .Meta.ip }}
 Имя узла: {{ .Meta.hostname }}
 Пользователь (инициатор): {{ .Meta.user_name }}
 Выполнена команда: {{ .Meta.command }}
-Путь выполнения: {{ .Meta.script_name }}
+Параметр команды: {{ .Meta.subcommand }}
+Путь выполнения: {{ .Meta.parent }}
 ]]
 
 -- Параметры группера
 local detection_window = "30s"
-local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.event.id"}
-local aggregated_by = {"target.image.name"}
+local grouped_by = {"observer.host.ip", "observer.host.hostname", "initiator.command.type"}
+local aggregated_by = {"initiator.command.executed"}
 local grouped_time_field = "@timestamp,RFC3339"
 
 -- Регулярные выражения, шаблоны
@@ -23,6 +24,9 @@ function on_logline(logline)
     local event_id = logline:gets("observer.event.id")
 
     if compare(event_id, "==", "4688") then
+        local command_executed = logline:gets("initiator.command.executed"):lower()
+        local command_parameter = command_executed:match("[%s\'\"]%w+::"):gsub("[%s:\'\"]", "") or "mimikatz"
+        set_field_value(logline, "initiator.command.type", command_parameter)
         grouper1:feed(logline)
     end
 end
@@ -37,10 +41,11 @@ function on_grouped(grouped)
         
         local command_executed = first_event:gets("initiator.command.executed")
         local initiator_name = first_event:gets("initiator.user.name", "Пользователь не определён")
-        local host_ip = first_event:get("observer.host.ip") or first_event:gets("reportchain.collector.host.ip", "IP-адрес не определён")
+        local host_ip = first_event:get("observer.host.ip") or first_event:gets("reportchain.collector.host.ip", "IP-адрес узла не определён")
         local host_name = first_event:gets("observer.host.hostname", "Имя узла не определено")
         local host_fqdn = first_event:gets("observer.host.fqdn", "FQDN узла не определено")
         local parent_path = first_event:gets("initiator.process.parent.path.original")
+        local subcommand = first_event:gets("initiator.command.type")
         
         if command_executed > 128 then
             command_executed = command_executed:sub(1, 128).. "... "
@@ -51,8 +56,8 @@ function on_grouped(grouped)
            meta = {
                 user_name=initiator_name,
                 command=command_executed,
+                subcommand=subcommand,
                 parent=parent_path,
-                file_name=file_path,
                 hostname=host_name,
                 ip=host_ip
                 },
