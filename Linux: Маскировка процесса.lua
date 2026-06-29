6cf974c619ead0cb59555077c39efead81cf8e26
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-06-29 12:58:19 +0300

    Rule a574c2ff-1329-49d6-a1ef-078a95c434c6 - the rule was remastered but inner test's failed.

--- Linux: Маскировка процесса.lua
+++ Linux: Маскировка процесса.lua
@@ -2,14 +2,13 @@ local template = [[
 Подозрение на маскировку процесса через SYSCALL vfork.
 
 ЦЕЛЕВОЙ УЗЕЛ:
-IP: {{.Meta.observer_ip}}
-Хост: {{.Meta.observer_hostname}}
+IP: {{ or .Meta.ip "IP-адрес не определён" }}
+Хост: {{ or .Meta.hostname "Имя узла не определено" }}
 FQDN: {{.Meta.observer_fqdn}}
 
 ИНИЦИАТОР:
 Пользователь: {{.Meta.user_name}}
-UID: {{.Meta.user_id}}
-Процесс: {{.Meta.process_path}}
+Процесс: {{.Meta.path}}
 Идентификатор родительского процесса: {{.Meta.parent_pid}}
 Идентификатор "дочернего" процесса: {{.Meta.child_pid}}
 
@@ -17,9 +16,15 @@ UID: {{.Meta.user_id}}
 {{.Meta.command}}
 ]]
 
-local detection_window = "1m"
-local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn"}
-local aggregated_by = {"observer.event.type"}
+local detection_window1 = "30s"
+local grouped_by1 = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn", "initiator.process.id", "initiator.process.parent.id"}
+local aggregated_by1 = {"target.syscall.name"}
+
+local detection_window2 = "30s"
+local grouped_by2 = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn", "event.process.id"}
+local aggregated_by2 = {"observer.event.type"}
+
+
 local grouped_time_field = "@timestamp,RFC3339"
 
 local suspicious_patterns = {   
@@ -50,83 +55,103 @@ function on_logline(logline)
     elseif event_type == "EXECVE" then
         local cmd = logline:gets("initiator.command.executed")
         if analyze(cmd) then
-            grouper1:feed(logline)
+            set_field_value(logline, "event.process.id", logline:gets("observer.process.id"))
+            grouper2:feed(logline)
         end
     end
 end
 
-function on_grouped(grouped)
+function on_grouped1(grouped)
     local events = grouped.aggregatedData.loglines
     local unique_events = grouped.aggregatedData.unique.total
-    local log_execve = nil
-    local log_vfork = nil
-    local log_syscall = nil
-    local vfork_count = 0
-    local syscall_count = 0
+    local log_sys_execve, log_sys_vfork
+    
+    log("Events: " ..#events.. ". Unique events: " ..unique_events)
+
+    if unique_events > 1 then
+        for _, event in ipairs(events) do
+            local syscall_name = event:gets("target.syscall.name"):lower()
+                
+            if syscall_name == "execve" then
+                log_sys_execve = event
+            else
+                log_sys_vfork = event
+            end
+        end
+
+        if log_sys_execve and log_sys_vfork then
+            local observer_pid = log_sys_execve:gets("observer.event.id")
+            set_field_value(log_sys_execve, "event.process.id", observer_pid)
+            set_field_value(log_sys_vfork, "event.process.id", observer_pid)
+            grouper2:feed(log_sys_execve)
+            grouper2:feed(log_sys_vfork)
+            grouper1:clear()
+        end
+
+    end
+end
+
+function on_grouped2(grouped)
+    local events = grouped.aggregatedData.loglines
+    local unique_events = grouped.aggregatedData.unique.total
+    local log_execve, log_vfork, log_syscall
 
     if unique_events > 1 then
 
-        for _, log in ipairs(events) do
+        for _, event in ipairs(events) do
             local event_type = log:gets("observer.event.type")
                         
             if event_type == "SYSCALL" then
-                local syscall_name = log:gets("target.syscall.name")
+                local syscall_name = log:gets("target.syscall.name"):lower()
                 
                 if syscall_name == "vfork" then
-                    log_vfork = log
-                    vfork_count = vfork_count + 1
+                    log_vfork = event
                 else
-                    log_syscall = log
-                    syscall_count = syscall_count + 1
+                    log_syscall = event
                 end
             elseif event_type == "EXECVE" then
-                log_execve = log
+                log_execve = event
             end
         end
 
-        if log_execve and log_vfork and log_syscall and vfork_count > 0 and syscall_count > 0 then
-            local initiator_id = log_vfork:gets("initiator.process.id")
-            local target_pid = log_syscall:gets("initiator.process.id")
-            local parent_pid = log_syscall:gets("initiator.process.parent.id")
+        if log_execve and log_vfork and log_syscall then
+            local command_executed = log_execve:gets("initiator.command.executed")
+            local process_path = log_vfork:gets("initiator.process.path.full")
+            local initiator_name = log_vfork:gets("initiator.user.name", "Имя пользователя не определено")
+            local host_ip = log_execve:gets("observer.host.ip")
+            local host_name = log_execve:gets("observer.host.hostname")
+            local host_fqdn = log_execve:gets("observer.host.fqdn")
+            local parent_id = log_syscall:gets("")
+                        
+            alert({
+                template = template,
+                meta = {
+                    ip = host_ip,
+                    hostname = host_name,
+                    user_name = initiator_name,
+                    path = process_path,
+                    command = command_executed,
+                    parent_pid = initiator_pid,
+                    child_pid = parent_pid
+                },
+                risk_level = 7.5,
+                asset_ip = host_ip,
+                asset_hostname = host_name,
+                asset_fqdn = host_fqdn,
+                asset_mac = "",
+                create_incident = true,
+                incident_group = "",
+                assign_to_customer = false,
+                incident_identifier = "",
+                logs = events,
+                mitre = {"T1036.004", "T1578.004", "T1564.007"},
+                trim_logs = 10
+            })
+            grouper2:clear()
             
-            if initiator_id == target_pid then
-                local command = log_execve:gets("initiator.command.executed")
-                local process_path = log_vfork:gets("initiator.process.path.full")
-                local user_name = log_vfork:gets("initiator.user.name") or "Имя пользователя не определено"
-                local user_id = log_vfork:gets("initiator.user.id") or "Идентификатор пользователя не определен"
-
-                   alert({
-                    template = template,
-                    meta = {
-                        observer_ip = log_execve:gets("observer.host.ip"),
-                        observer_hostname = log_execve:gets("observer.host.hostname"),
-                        observer_fqdn = log_execve:gets("observer.host.fqdn"),
-                        user_name = user_name,
-                        user_id = user_id,
-                        process_path = process_path,
-                        command = command,
-                        parent_pid = initiator_pid,
-                        child_pid = parent_pid,
-                        suspicious_path = command,
-                        detection_count = tostring(vfork_count)
-                    },
-                    risk_level = 7.5,
-                    asset_ip = log_execve:get_asset_data("observer.host.ip"),
-                    asset_hostname = log_execve:get_asset_data("observer.host.hostname"),
-                    asset_fqdn = log_execve:get_asset_data("observer.host.fqdn"),
-                    asset_mac = "",
-                    create_incident = true,
-                    incident_group = "",
-                    assign_to_customer = false,
-                    incident_identifier = "",
-                    logs = events,
-                    mitre = {"T1036.004", "T1578.004", "T1564.007"},
-                    trim_logs = 10
-                })
-                grouper1:clear()
-            end
         end
     end
 end
 
-grouper1 = grouper.new(grouped_by, aggregated_by, grouped_time_field, detection_window, on_grouped)
+grouper1 = grouper.new(grouped_by1, aggregated_by1, grouped_time_field, detection_window1, on_grouped1)
+grouper2 = grouper.new(grouped_by2, aggregated_by2, grouped_time_field, detection_window2, on_grouped2)
