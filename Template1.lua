f8a0ddcae68fa3d064d538620a2cf6dca02d606f
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-07-06 13:26:57 +0300

    Rule 1875c38c-a4d9-4277-9eae-d6499507659a - the rule was overwritten.

---
+++ Template1.lua
@@ -0,0 +1,120 @@
+-- Шаблон алерта
+local template = [[
+Подозрение на маскировку процесса через SYSCALL vfork и SYSCALL clone.
+
+ЦЕЛЕВОЙ УЗЕЛ:
+IP: {{ or .Meta.ip "IP-адрес не определён" }}
+Хост: {{ or .Meta.hostname "Имя узла не определено" }}
+FQDN: {{ or .Meta.observer_fqdn "FQDN узла не определено" }}
+
+ИНИЦИАТОР:
+Пользователь: {{ or .Meta.user "Пользователь не определён"}}
+
+ВЫПОЛНЕННАЯ КОМАНДА:
+{{ .Meta.command }}
+Процесс/Путь к имполняемому файлу: {{ .Meta.path }}
+]]
+
+-- Параметры группера
+local detection_window = "30s"
+local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn"}
+local aggregated_by = {"target.syscall.name"}
+local grouped_time_field = "@timestamp,RFC3339"
+
+-- Регулярные выражения
+local prefix = "(?:^|\\/|\\s+|\"|\'|\\()"
+local suffix = "(?:$|\\/|\\s+|\"|\'|\\))"
+local suspicious_patterns = {   
+                        
+}
+
+-- Функция сокразения строки для алерта
+local function string_cut(cmd)
+    if #cmd > 128 then
+        cmd = cmd:sub(1, 128).. "... "
+    end
+
+    return cmd
+end
+
+-- Функция анализа строки по регулярному выражению
+local function analyze(cmd)
+    local cmd_lower = cmd:lower()
+    for _, pattern in ipairs(suspicious_patterns) do
+        if cmd_lower:search(pattern) then
+            return true
+        end
+    end
+    return false
+end
+
+-- Функция обработки логлайна
+function on_logline(logline)
+    local event_type = logline:gets("observer.event.type")
+    local event_id = logline:gets("observer.event.id")
+    ...
+    grouper1:feed(logline)
+end
+
+-- Функция группера #1
+function on_grouped1(grouped)
+    local events = grouped.aggregatedData.loglines
+    local unique_events = grouped.aggregatedData.unique.total
+    local log_1, log_2
+    
+    if unique_events > 1 then
+        for _, event in ipairs(events) do
+            local parameter = event:gets("..."):lower()
+
+            if syscall_name == "" then
+                log_1 = event
+            else
+                log_2 = event
+            end
+        end
+
+        if log_1 and log_2 then
+            local initiator_name = log_1:gets("initiator.user.name")  
+            local host_ip = log_2:get("observer.host.ip")
+            local host_name = log_2:gets("observer.host.hostname")
+            local host_fqdn = log_2:gets("observer.host.fqdn")
+            local program_name = log_1:gets("target.image.name")
+            local command_executed = log_2:gets("initiator.command.executed")
+            local process_path = log_2:get("target.process.path.full")
+
+            if #command_executed > 255 then
+                command_executed = command_executed:sub(1,255) .. "... "
+            end
+                        
+            alert({
+               template = template,
+               meta = {
+                   user=initiator_name,
+                   initiator_command=command_executed,
+                   path=process_path,
+                   program=program_name,
+                   ip=host_ip,
+                   hostname=host_name
+                   },
+               risk_level = 4.0, 
+               asset_ip = host_ip,
+               asset_hostname = host_name,
+               asset_fqdn = host_fqdn,
+               asset_mac = "",
+               create_incident = true,
+               incident_group = "",
+               assign_to_customer = false,
+               incident_identifier = "",
+               logs = events,
+               mitre = {"T1036.004"},
+               trim_logs = 10
+               }
+            )
+            grouper1:clear()
+        end
+
+    end
+end
+
+
+
---
+++ Windows - Подозрительные манипуляции с теневыми копиями.lua
@@ -0,0 +1,143 @@
+-- Шаблон алерта
+local template = [[
+{{ Meta.title }}.
+
+ЦЕЛЕВОЙ УЗЕЛ:
+IP: {{ or .Meta.ip "IP-адрес не определён" }}
+Хост: {{ or .Meta.hostname "Имя узла не определено" }}
+FQDN: {{ or .Meta.observer_fqdn "FQDN узла не определено" }}
+
+ИНИЦИАТОР:
+Пользователь: {{ or .Meta.user "Пользователь не определён"}}
+
+ВЫПОЛНЕННАЯ КОМАНДА:
+{{ .Meta.command }}
+Путь к имполняемому файлу: {{ .Meta.path }}
+Имя службы: {{ .Meta.service }}
+Статус задачи: {{ .Meta.status }}
+]]
+
+-- Параметры группера
+local detection_window = "30s"
+local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn"}
+local aggregated_by = {"target.syscall.name"}
+local grouped_time_field = "@timestamp,RFC3339"
+
+-- Регулярные выражения
+local patterns = {
+    VSSADMIN = {
+        short_pattern = "vssadmin"
+        main_pattern = [["[^"]*vssadmin(?:\.exe)?"?\s+(?:create\s+shadow|delete\s+shadows|list\s+shadows)\b.*]],
+        name = "Обнаружено создание, удаление или перечисление теневых копий Volume Shadow Copy"
+    },
+    DISKSHADOW = {
+        short_pattern = "diskshadow"
+        main_pattern = [["?[^"]*diskshadow(?:\.exe)?"?\s*(?:\/s\s+\S+|.*\b(create|expose|delete|list)\b.*)]],
+        name = "Обнаружено использование DiskShadow для управления теневыми копиями Volume Shadow Copy"
+    },
+    WMIC = {
+        short_pattern = "wmic"
+        main_pattern = [[ "?[^"]*wmic(?:\.exe)?"?\s+shadowcopy\s+(?:call\s+create|delete|list)(?:\/|\s+|"|')[\s\S]* ]],
+        name = "Обнаружено использование WMIC для создания, удаления или перечисления теневых копий Volume Shadow Copy"
+    }
+}
+
+-- Функция сокразения строки для алерта
+local function string_cut(cmd)
+    if #cmd > 128 then
+        cmd = cmd:sub(1, 128).. "... "
+    end
+
+    return cmd
+end
+
+-- Функция анализа строки по регулярному выражению
+local function analyze(cmd)
+    local cmd = cmd:lower()
+    for _, pattern in ipairs(patterns) do
+        if substr(cmd, pattern.short_pattern) then
+            if cmd:sarch(pattern.main_pattern) then
+                local title = pattern.name
+                return title, true
+            end
+        end
+    end
+    
+    return false
+end
+
+-- Функция обработки логлайна
+function on_logline(logline)
+    local title
+    local event_id = logline:gets("observer.event.id")
+
+
+    if compare(event_id, "==", "4688") then
+        title, is_vss = analyze(logline:gets("initiator.command.executed"))
+
+        if is_vss then
+            set_field_value(logline, "event.rule.description", title)
+            grouper1:feed(logline)
+        end
+    else
+        grouper1:feed(logline)
+    end
+end
+
+-- Функция группера #1
+function on_grouped1(grouped)
+    local events = grouped.aggregatedData.loglines
+    local unique_events = grouped.aggregatedData.unique.total
+    local log_exec, log_service
+    
+    if unique_events > 1 then
+        for _, event in ipairs(events) do
+            if compare(event:gets("observer.event.id"), "==", "4688") then
+                log_exec = event
+            else
+                log_service = event
+            end
+        end
+
+        if log_exec and log_service then
+            local initiator_name = log_exec:gets("initiator.user.name")  
+            local host_ip = log_exec:gets("observer.host.ip")
+            local host_name = log_exec:gets("observer.host.hostname")
+            local host_fqdn = log_exec:gets("observer.host.fqdn")
+            local service_name = log_service:gets("target.service.name")
+            local command_executed = string_cut(log_exec:gets("initiator.command.executed"))
+            local process_path = log_exec:gets("target.process.path.full")
+            local task_status = log_service:gets("target.task.status.name")
+            local description = log_exec:gets("event.rule.description")
+
+            alert({
+               template = template,
+               meta = {
+                   title=description,
+                   user=initiator_name,
+                   command=command_executed,
+                   path=process_path,
+                   service=service_name,
+                   status=task_status,
+                   ip=host_ip,
+                   hostname=host_name
+                   },
+               risk_level = 7.0, 
+               asset_ip = host_ip,
+               asset_hostname = host_name,
+               asset_fqdn = host_fqdn,
+               asset_mac = "",
+               create_incident = true,
+               incident_group = "",
+               assign_to_customer = false,
+               incident_identifier = "",
+               logs = events,
+               mitre = {"T1003.003", "T1490"},
+               trim_logs = 10
+               }
+            )
+            grouper1:clear()
+        end
+
+    end
+end
