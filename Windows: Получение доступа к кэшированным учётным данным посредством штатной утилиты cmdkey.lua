559b285e4253d1441a92fd0ce5a492d838ae1a97
Ilya Shmadchenko <prophet8912@yahoo.com>
2026-07-10 14:59:39 +0300

    Rule 05855518-6390-a02e-2a47-11d533826449 - the rule has been added.

--- Windows: Получение доступа к кэшированным учётным данным посредством штатной утилиты cmdkey.lua
+++ Windows: Получение доступа к кэшированным учётным данным посредством штатной утилиты cmdkey.lua
@@ -1,6 +1,6 @@
 -- Шаблон алерта
 local template = [[
-Подозрение на маскировку процесса через SYSCALL vfork и SYSCALL clone.
+Обнаружено получение доступа к кэшированным учётным данным посредством штатной утилиты cmdkey.
 
 ЦЕЛЕВОЙ УЗЕЛ:
 IP: {{ or .Meta.ip "IP-адрес не определён" }}
@@ -13,21 +13,17 @@ FQDN: {{ or .Meta.fqdn "FQDN узла не определено" }}
 ВЫПОЛНЕННАЯ КОМАНДА:
 {{ .Meta.command }}
 Имя программы: {{ .Meta.program }}
-Процесс/Путь к иcполняемому файлу: {{ .Meta.path }}
+Процесс/Путь к исполняемому файлу: {{ .Meta.path }}
 ]]
 
 -- Параметры группера
 local detection_window = "30s"
 local grouped_by = {"observer.host.ip", "observer.host.hostname", "observer.host.fqdn"}
-local aggregated_by = {"target.syscall.name"}
+local aggregated_by = {"observer.event.id"}
 local grouped_time_field = "@timestamp,RFC3339"
 
 -- Регулярные выражения
-local prefix = "(?:^|\\/|\\s+|\"|\'|\\()"
-local suffix = "(?:$|\\/|\\s+|\"|\'|\\))"
-local suspicious_patterns = {   
-                        
-}
+local suspicious_pattern = [[(?:^|\/|\s+|"|'|\()cmdkey(\.exe)?\s+\/?list]]
 
 -- Вспомогательная функция логирования значений в группере (удалить после тестирования на потоке)
 local function log_grouper(events, events_number, unique_events, grouper_name, grouper_field)
@@ -41,7 +37,7 @@ local function log_grouper(events, events_number, unique_events, grouper_name, g
 end
 
 -- Функция алерта
-local function alert_function(events, ip, hostname, fqdn, user, cmd, program, service, path, source, destination)
+local function alert_function(events, ip, hostname, fqdn, user, cmd, program, path)
     alert({
         template = template,
         meta = {
@@ -50,13 +46,11 @@ local function alert_function(events, ip, hostname, fqdn, user, cmd, program, se
             path=path,
             program=program,
             service=service,
-            source_file=source,
-            destination_file=destination,
             ip=ip,
             hostname=hostname,
             fqdn=fqdn
             },
-        risk_level = 4.0, 
+        risk_level = 9.0, 
         asset_ip = ip,
         asset_hostname = hostname,
         asset_fqdn = fqdn,
@@ -66,7 +60,7 @@ local function alert_function(events, ip, hostname, fqdn, user, cmd, program, se
         assign_to_customer = false,
         incident_identifier = "",
         logs = events,
-        mitre = {"T1003.004"},
+        mitre = {"T1003.005"},
         trim_logs = 10
         }
      )
@@ -81,56 +75,34 @@ local function string_cut(cmd)
     return cmd
 end
 
--- Функция анализа строки по регулярному выражению
-local function analyze(cmd)
-    local cmd_lower = cmd:lower()
-    for _, pattern in ipairs(suspicious_patterns) do
-        if cmd_lower:search(pattern) then
-            return true
-        end
-    end
-    return false
-end
-
 -- Функция обработки логлайна
 function on_logline(logline)
-    local event_type = logline:gets("observer.event.type")
-    local event_id = logline:gets("observer.event.id")
-    ...
-    grouper1:feed(logline)
+    local command_executed = logline:gets("initiator.command.executed"):lower()
+    
+    if command_executed:search(suspicious_pattern) then
+        grouper1:feed(logline)
+    end
 end
 
 -- Функция группера #1
 function on_grouped(grouped)
     local events = grouped.aggregatedData.loglines
-    local unique_events = grouped.aggregatedData.unique.total
-    local log_1, log_2
+    local first_event = events[1]
     
 --    log_grouper(events, #events, unique_events, "on_grouped", grouped_by[4])
 
-    if unique_events > 1 then
-        for _, event in ipairs(events) do
-            local parameter = event:gets("..."):lower()
+    if #events > 0 then
+        local initiator_name = first_event:gets("initiator.user.name")  
+        local host_ip = first_event:get("observer.host.ip")
+        local host_name = first_event:gets("observer.host.hostname")
+        local host_fqdn = first_event:gets("observer.host.fqdn")
+        local program_name = first_event:gets("target.image.name")
+        local command_executed = string_cut(first_event:gets("initiator.command.executed"))
+        local process_path = first_event:get("target.process.path.full")
 
-            if syscall_name == "" then
-                log_1 = event
-            else
-                log_2 = event
-            end
-        end
-
-        if log_1 and log_2 then
-            local initiator_name = log_1:gets("initiator.user.name")  
-            local host_ip = log_2:get("observer.host.ip")
-            local host_name = log_2:gets("observer.host.hostname")
-            local host_fqdn = log_2:gets("observer.host.fqdn")
-            local program_name = log_1:gets("target.image.name")
-            local command_executed = string_cut(log_2:gets("initiator.command.executed"))
-            local process_path = log_2:get("target.process.path.full")
-
-            alert_function(events, host_ip, host_name, host_fqdn, initiator_name, command_executed, program_name, service_name, process_path, source_path, output_path)
-            grouper1:clear()
-        end
+        alert_function(events, host_ip, host_name, host_fqdn, initiator_name, command_executed, program_name, process_path)
+        grouper1:clear()
+        
 
     end
 end
